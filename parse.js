f05677a68b88f008e2d8303bfbcd424f3405eb19
yus-ki <1327177559@qq.com>
2026-04-28 13:40:09 +0800

    Delete parse.js

--- parse.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const acorn = require('acorn');
-const jsx = require('acorn-jsx');
-
-const Parser = acorn.Parser.extend(jsx());
-const code = fs.readFileSync('src/App.tsx', 'utf8');
-
-// I will just use typescript compiler API to parse the AST!
